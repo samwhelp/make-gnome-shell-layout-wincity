10f4c8a628ea12af126a3ead8725f53594789850
samwhelp <samwhelp@users.noreply.github.com>
2026-02-21 18:42:41 +0800

    fix

--- part/make-gnome-shell-layout/make-gnome-shell-layout.sh
+++ part/make-gnome-shell-layout/make-gnome-shell-layout.sh
@@ -16,6 +16,46 @@
 
 
 ##
+## ## Option
+##
+
+REF_GNOME_SHELL_EXTENSIONS_INSTALL=(
+	"dash-to-panel@jderose9.github.com"
+	"arcmenu@arcmenu.com"
+	"blur-my-shell@aunetx"
+	"date-menu-formatter@marcinjakubowski.github.com"
+	"user-theme@gnome-shell-extensions.gcampax.github.com"
+	"status-icons@gnome-shell-extensions.gcampax.github.com"
+	"gtk4-ding@smedius.gitlab.com"
+)
+
+
+
+
+REF_GNOME_SHELL_EXTENSIONS_ENABLE=(
+	"dash-to-panel@jderose9.github.com"
+	"arcmenu@arcmenu.com"
+	"blur-my-shell@aunetx"
+	"date-menu-formatter@marcinjakubowski.github.com"
+	"user-theme@gnome-shell-extensions.gcampax.github.com"
+	"status-icons@gnome-shell-extensions.gcampax.github.com"
+	"gtk4-ding@smedius.gitlab.com"
+)
+
+
+
+
+REF_GNOME_SHELL_EXTENSIONS_DISABLE=(
+	"dash-to-dock@micxgx.gmail.com"
+	"apps-menu@gnome-shell-extensions.gcampax.github.com"
+	"places-menu@gnome-shell-extensions.gcampax.github.com"
+	"window-list@gnome-shell-extensions.gcampax.github.com"
+)
+
+
+
+
+##
 ## ## Args
 ##
 
