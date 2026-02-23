6a3eb30b4c247f46479dcfbef0656dedea3fd5d1
samwhelp <samwhelp@users.noreply.github.com>
2026-02-23 12:42:31 +0800

    fix

--- part/make-gnome-shell-style/make-gnome-shell-style.sh
+++ part/make-gnome-shell-style/make-gnome-shell-style.sh
@@ -1637,6 +1637,107 @@ sys_theme_install_orchis_gtk_theme_via_wget_archive () {
 
 
 ##
+## ## Model / Style / layan
+##
+
+sys_style_install_for_layan () {
+
+	sys_style_asset_install_for_layan
+
+	sys_style_config_install_for_layan
+
+}
+
+sys_style_config_install_for_layan () {
+
+dconf load / << __EOF__
+
+
+[org/gnome/desktop/interface]
+color-scheme='prefer-dark'
+gtk-theme='Layan-Dark'
+icon-theme='Citrus-red-dark'
+cursor-theme='Vimix-cursors'
+cursor-size=24
+
+
+[org/gnome/desktop/wm/preferences]
+theme='Layan-Dark'
+
+
+[org/gnome/shell/extensions/user-theme]
+name='Layan-Dark'
+
+
+__EOF__
+
+}
+
+sys_style_asset_install_for_layan () {
+
+	sys_wallpaper_install_for_default
+
+	sys_theme_install_layan_gtk_theme
+
+	sys_theme_install_vimix_cursor_theme
+
+	sys_theme_install_citrus_icon_theme
+
+	sys_theme_install_greystone_icon_theme
+
+	sys_theme_install_questx_icon_theme
+
+}
+
+sys_theme_install_layan_gtk_theme () {
+
+	sys_theme_install_layan_gtk_theme_via_wget_archive
+
+}
+
+sys_theme_install_layan_gtk_theme_via_wget_archive () {
+
+
+	if [ -e "${HOME}/.themes/Layan-Dark" ]; then
+		return 0
+	fi
+
+
+	wget -c 'https://github.com/vinceliuice/Layan-gtk-theme/archive/refs/heads/master.tar.gz' -O '/tmp/Layan-gtk-theme-master.tar.gz'
+
+
+
+
+	cd /tmp
+
+	tar xf Layan-gtk-theme-master.tar.gz
+
+	cd "${OLDPWD}"
+
+
+
+
+	cd /tmp/Layan-gtk-theme-master
+
+	./install.sh
+
+	cd "${OLDPWD}"
+
+}
+
+
+
+
+##
+##
+################################################################################
+##
+##
+
+
+
+
+##
 ## ## Portal
 ##
 
